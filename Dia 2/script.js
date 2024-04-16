0652ec1816fe2fbea6c59511fe44d673707894b5
Santiago <j.santiagolseon@gmail.com>
2024-04-16 10:22:36 -0500

    feat: :construction: Menu Trainers
    Se implementan nuevas funcionalidades al menu, falta terminar

--- Dia 2/script.js
+++ Dia 2/script.js
@@ -1,6 +1,6 @@
 let campers = [];
 
-let Trainers = ["1. Pedro", "2. Jholber", "3. Miguel", "4. Garcia", "5. Fabio", "6. Holguer"]
+let Trainers = [];
 
 //Bucle del programa para lograr continuidad
 while (true) {
@@ -81,11 +81,49 @@ while (true) {
         //Menú de Trainers
         rta1 = prompt("Trainers activos:\n \n" + Trainers +"\n \nIngrese el número de la opcion a la que desea ingresar\n1. Informacion del Trainer\n2. Eliminar Trainer\n3. Agregar Trainer\n4. Reemplazar Trainer\n5. Volver al Menu Principal")
         if (rta1 == 1){
-            //Mostrar informacion del Trainer seleccionado
-            selectTrainer = prompt("Ingrese el numero del trainer que desea ver:" + Trainers)
-        } if (selectTrainer == 1){
-            prompt("Nombre: Pedro\nSalones asignados: P1, P2\nHorario: 6:00 - 9:30; 10:00 - 14:00\nRuta asignada: Ruta Java\n quiere hacer pull o push?")
-        } else if (rta1 == 5) {
+                //Mostrar informacion del Trainer seleccionado
+                selectTrainer = parseInt(prompt("Ingrese el numero del trainer que desea ver:" + Trainers));
+            if (selectTrainer >= 1 && selectTrainer <= Trainers.length){
+                let trainerInfo = obtenerInformacionTrainer(selectTrainer - 1);
+                alert(trainerInfo);
+            } else {
+                alert("Número de Trainers no válido");
+            }
+        } else if (rta1 == 2){
+            let eliminarTrainer = parseInt(prompt("Ingrese el numero del trainer que desea eliminar: " + Trainers));
+            if (eliminarTrainer >= 1 && eliminarTrainer <= Trainers.length) {
+                //Eliminar Trainer
+                Trainers.splice(eliminarTrainer - 1, 1);
+                alert("Trainer eliminado correctamente")
+            } else{
+                alert("Número de Trainer Incorrecto")
+            }
+        } else if (rta1 == 3){
+            let cantidadTrainers = parseInt(prompt("¿Cuantos Trainers desea agregar? "))
+            for (let i = 0; i < cantidadTrainers; i++){
+                trainer = {
+                    id: 0,
+                    nombres: "",
+                    apellidos: "",
+                    direccion: "",
+                    contacto: 0,
+                    salon: "",
+                    ruta: "",
+                }
+                Trainers.push(trainer)
+
+        }
+    } else if ( rta1 == 4){
+        let reemplazarTrainer = parseInt(prompt("Ingrese el numero del trainer el cual desea reemplazar: " + Trainers));
+        if (reemplazarTrainer >= 1 && reemplazarTrainer <= Trainers.length) {
+            let nuevoTrainer = prompt("Ingrese los datos del nuevo Trainer")
+            Trainers[reemplazarTrainer - 1] = nuevoTrainer
+            alert("Trainer reemplazado correctamente ");
+        } else {
+            alert("Numero de trainer No Valido");
+        }
+    }
+        else if (rta1 == 5) {
             continue; //Volver al menú principal
         }
 
