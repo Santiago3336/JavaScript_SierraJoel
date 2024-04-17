7edc9d5dcc45a6ad26916e011f9eb476d0115d69
Santiago <j.santiagolseon@gmail.com>
2024-04-17 07:37:01 -0500

    feat: :sparkles: Solucion ejercicio BST
    Implementacion de codigo para la solucion del ejemplo 1 del BST trabajado en clase

---
+++ Dia 3/script.js
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function(root, key) {
+    if (!root) return null;
+
+    if (key < root.val) {
+        root.left = deleteNode(root.left, key);
+    } else if (key > root.val) {
+        root.right = deleteNode(root.right, key);
+    } else {
+        if (!root.left) return root.right;
+        else if (!root.right) return root.left;
+
+        let successor = root.right;
+        while (successor.left) {
+            successor = successor.left;
+        }
+
+        root.val = successor.val;
+
+        root.right = deleteNode(root.right, successor.val);
+    }
+
+    return root;
+};
+
+function createAndDeletNode() {
+    function TreeNode(val, left, right) {
+        this.val = (val === undefined ? 0 : val);
+        this.left = (left === undefined ? null : left);
+        this.right = (right === undefined ? null : right);
+    }
+
+    let root = new TreeNode(5);
+    root.left = new TreeNode(3);
+    root.right = new TreeNode(6);
+    root.left.left = new TreeNode(2);
+    root.left.right = new TreeNode(4);
+    root.right.right = new TreeNode(7);
+
+    
+    let result = deleteNode(root, 3);
+    console.log(result);
+}
+
+
+createAndDeletNode();
