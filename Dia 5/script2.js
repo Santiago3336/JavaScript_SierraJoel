0d651bb41b8e462fbc56bb992c72fdee9dbf4e36
Santiago <j.santiagolseon@gmail.com>
2024-04-21 20:14:10 -0500

    feat: :bulb: Añadir comentarios
    Se añaden comentarios para comprension del codigo

--- Dia 5/script2.js
+++ Dia 5/script2.js
@@ -1,3 +1,4 @@
+//Definir la clase rectangle
 class rectangle {
     constructor(widht, height){
         this.widht = widht;
@@ -6,17 +7,20 @@ class rectangle {
         }
 
 
-
+//Calcular el area
 area() {
     return this.widht * this.height;
 }
 
+//Calcular el perimetro
 perimeter() {
     return 2 * (this.widht + this.height);
 }
 }
 
+//Creacion de la instancia para la clase rectangle
 let Rectangle = new rectangle(5, 10);
 
+//Calcular y mostrar el Area y el perimetro
 console.log("Area: " + Rectangle.area());
 console.log("Perimeter: " + Rectangle.perimeter());
